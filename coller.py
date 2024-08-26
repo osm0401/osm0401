856ada57327c16b6942a73a29955ec2d6dbb0d42
osm0401 <168968093+osm0401@users.noreply.github.com>
2024-08-26 23:05:14 +0900

    Update coller.py

--- coller.py
+++ coller.py
@@ -1,1 +1,24 @@
-print("a")
+import time
+import openai
+import requests
+from bs4 import BeautifulSoup
+openai.api_key = ""
+
+messages = []
+for i in range(1,10):
+    k = "https://codeup.kr/problem.php?id="
+    num = 6063
+    a = requests.get(k + str(num+i))
+    kk = BeautifulSoup(a.content, 'html.parser')
+    found = kk.find(id="pro1")
+    #user_content = input("user : ")
+    messages.append({"role": "user", "content": f"{str(found)+"python정답코드만짜줘"}"})
+    completion = openai.ChatCompletion.create(model="gpt-4o", messages=messages)
+    assistant_content = completion.choices[0].message["content"].strip()
+    messages.append({"role": "assistant", "content": f"{assistant_content}"})
+    print(f"GPT : {assistant_content}")
+    print(a)
+    print()
+    print()
+    print()
+    time.sleep(2)

48faba7b76565d9a469f538862dbb963af4f89d8
osm0401 <168968093+osm0401@users.noreply.github.com>
2024-12-13 22:13:19 +0900

    Update discord_cracker.py

--- discord_cracker.py
+++ discord_cracker.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
@@ -14,18 +16,20 @@ driver = webdriver.Edge(options=options)
 a = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
 ass = "https://discord.com/gifts/"
 
-for _ in range(100):
+for _ in range(10000):
     q = "".join(random.choice(a) for _ in range(16))
     qw = ass + q
 
     try:
         driver.get(qw)
-        time.sleep(2)
-
+        sleep(1)
         try:
             element = driver.find_element(By.CSS_SELECTOR, ".text-md\\2f normal_dc00ef")
             if element.text == "선물 코드가 만료되었거나 잘못 입력하신 거 같아요.":
                 print(f"Code expired or invalid: {q}")
+            else:
+                print(f"succes : P{q}")
+                exit()
         except Exception:
             print(f"Element not found for code: {q}")
     except Exception as e:
